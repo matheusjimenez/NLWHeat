54fe5347fca91e85cb910e4755e7d3284febd8fe
Matheus Jimenez <matheus.jimenez@hotmail.com.br>
2021-10-18 23:05:34 -0300

    NLW first class

---
+++ src/controllers/ProfileUserController.ts
@@ -0,0 +1,19 @@
+import {Request, Response} from "express";
+import { AuthenticateUserService } from "../services/AuthenticateUserService";
+import { CreateMessageService } from "../services/CreateMessageService";
+import { GetLast3MessagesService } from "../services/GetLast3MessagesService";
+import { ProfileUserService } from "../services/ProfileUserService";
+
+class ProfileUserController{
+    async handle(request: Request, response: Response) {
+        const { user_id } = request;
+
+        const service = new ProfileUserService();
+
+        const result = await service.execute(user_id);
+
+        return response.json(result);
+    }
+}
+
+export { ProfileUserController }
---
+++ src/server.ts
@@ -0,0 +1,5 @@
+import { serverHttp } from "./app";
+
+
+serverHttp.listen(4000, () => console.log(`🎈 server is running on PORT 4000`));
+
